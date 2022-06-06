57b33d140b1811e5fe6dcedee0d748f4d0a570ab
Jean Paul Ruiz <jpruiz114@gmail.com>
2022-06-06 11:04:27 -0400

    Adding changes to refresh token

--- html/assets/js/views/transformations.js
+++ html/assets/js/views/transformations.js
@@ -26,13 +26,49 @@ function validateJwt(accessToken) {
     $.ajax(settings).done(function (response) {
         // do something specific to the transformations view, such as loading transformations per user
 
-        let parsedJwt = parseJwt(accessToken);
-        console.dir(parsedJwt);
+        setInterval(refreshTimer, 10000);
     });
 
     $.ajax(settings).fail(function (response) {
         document.location = 'login.php';
     });
+}
+
+function refreshTimer() {
+    let inFiveMin = Math.floor((Date.now() + (5 * 60 * 1000))  / 1000);
+
+    let accessToken = getCookie('access_token');
+    let parsedJwt = parseJwt(accessToken);
+    let jwtExpiresAt = parsedJwt.eat;
+
+    // If the token expires in 5 minutes or less, refresh the token
+    if (inFiveMin >= jwtExpiresAt) {
+        let refreshToken = getCookie('refresh_token');
+
+        const form = new FormData();
+        form.append("access_token", accessToken);
+        form.append("refresh_token", refreshToken);
+
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:8181/api/v1/refresh-token",
+            "method": "POST",
+            "processData": false,
+            "contentType": false,
+            "mimeType": "multipart/form-data",
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            //console.log(response);
+
+
+        });
+    }
+    else {
+        console.log('JWT cannot be refreshed yet. No 5 minute frame yet.')
+    }
 }
 
 $(document).ready(function () {
